a4b35553834fbbd757708b979c1627cfb8be5b92
Jonathan <Jonathanconn29@gmail.com>
2019-02-26 23:50:13 -0500

    submit on enter for textarea   nextline func to read on submit

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-function test(){
-document.getElementById('term').innerHTML = "New Text";
+var text = "";
+
+//text area setup onwindow load, called from body
+function start(){
+  text = "C:\\";
+  document.getElementById("term").innerHTML = text;
 }
+
+//when enter key pressed in text area it submits the form
+function submitOnEnter(event){
+    if(event.which === 13 && !event.shiftKey){
+        event.target.form.dispatchEvent(new Event("submit", {cancelable: true}));
+        // event.preventDefault(); prevents next line being created
+        console.log("submitted");
+        nextLine();
+    }
+}
+
+//edit text area after submit
+function nextLine(){
+  var command = document.getElementById("term").value;
+  command = command.substring(3);
+  console.log(command);
+
+
+
+}
+
+
+//for submit on enter
+document.getElementById("term").addEventListener("keypress", submitOnEnter);
--- indexJS.js
+++
@@ -1,6 +0,0 @@
-var wage = document.getElementById("outTERM");
-wage.addEventListener("keydown", function (e) {
-    if (e.keyCode === 13) {
-      
-    }
-});
