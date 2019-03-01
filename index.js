bf35fa3758200f8251e5a9ce0170e96019883c61
Jonathan <Jonathanconn29@gmail.com>
2019-03-01 00:45:38 -0500

    initial setup of command handling and data structure for sim directory

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var text = "";
+var comList = ["cd", "dir"];
+
+//for submit on enter
+document.getElementById("term").addEventListener("keypress", submitOnEnter);
 
 //text area setup onwindow load, called from body
 function start(){
@@ -11,21 +15,83 @@ function submitOnEnter(event){
     if(event.which === 13 && !event.shiftKey){
         event.target.form.dispatchEvent(new Event("submit", {cancelable: true}));
         // event.preventDefault(); prevents next line being created
-        console.log("submitted");
-        nextLine();
+        checkCom();
     }
 }
 
 //edit text area after submit
-function nextLine(){
+function checkCom(){
   var command = document.getElementById("term").value;
-  command = command.substring(3);
+  command = command.substring(3, command.indexOf(" "));
+
   console.log(command);
 
+  if(contains(comList, command)) runCom(command);
+  else printError(command);
 
+}
+
+function runCom(command){
+    console.log("running " + command + " :) ");
+}
+
+function printError(command){
+  console.log("ERROR " + command + " NOT FOUND");
+  newLine();
+}
+
+function newLine(){
 
 }
 
 
-//for submit on enter
-document.getElementById("term").addEventListener("keypress", submitOnEnter);
+function contains(array, command){
+  for(var i = 0; i < comList.length; i++)
+    if(comList[i] == command.toLowerCase())
+      return true;
+  return false;
+}
+
+
+class Tree{
+
+  constructor(){
+
+  }
+
+  Node(){
+    this.data = data;
+    this.parent = null;
+    this.children = [];
+  }
+
+  Tree(data){
+    var node = new Node(data);
+    this._root = node;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
